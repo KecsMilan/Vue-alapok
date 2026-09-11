e6a9b4a76804b72a1f7ec166b437a8c30a4563ba
KecsMilan <kecskemeti.milan@diak.szbi-pg.hu>
2026-09-11 09:38:36 +0200

    Upload
    vue basics ( v-for, v-if, v-show, style-binding )

---
+++ 01-Syntax/09-v-style-binding/main.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    setup() {
+        const guitars = [
+            {id: 1, name: "Fender Stratocaster", stock: 0},
+            {id: 2, name: "Fender Foschater", stock: 3},
+            {id: 3, name: "Bendy McRul", stock: 5},
+            {id: 4, name: "Albert Istfhisch", stock: 1},
+            {id: 5, name: "Eclipse McLipse", stock: 10},
+        ];
+
+        const outOfStockColor = "red";
+        const onlyFewLeftColor = "orange";
+        const inStockColor = "lightGreen";
+
+        return { guitars, outOfStockColor, onlyFewLeftColor, inStockColor };
+    }
+});
+
+app.mount('#appdiv');
